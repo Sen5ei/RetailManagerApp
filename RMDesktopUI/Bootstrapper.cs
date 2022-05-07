bf7239a769763d3d4f1fc8e2222c2e48b245b528
Ognjen Sredic <nsredic@sbb.rs>
2022-05-07 16:21:44 +0200

    Add WPF project with MVVM
    Adds our WPF UI project and does basic CaliburnMicro wiring.

---
+++ RMDesktopUI/Bootstrapper.cs
@@ -0,0 +1,27 @@
+﻿using Caliburn.Micro;
+using RMDesktopUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace RMDesktopUI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            // Start ShellViewModel on startup, which will then launch the ShellView.xaml as our base view
+            // (based upon CaliburnMicro wiring those two things together),
+            // since we deleted MainWindow.xaml and adjusted App.xaml
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
