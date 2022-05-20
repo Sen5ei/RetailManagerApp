10005665ed78fcdf1223a134bbc5429580512b9a
Ognjen Sredic <nsredic@sbb.rs>
2022-05-20 23:41:20 +0200

    Wire Up the API (Getting user data)
    Adds UserController with GetById() method which returns user information.

---
+++ RMDataManager/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNet.Identity;
+using RMDataManager.Library.DataAccess;
+using RMDataManager.Library.Models;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace RMDataManager.Controllers
+{
+    [Authorize]
+    public class UserController : ApiController
+    {
+        public List<UserModel> GetById()
+        {
+            // Get the Id of the user who is logged in
+            string userId = RequestContext.Principal.Identity.GetUserId();
+
+            UserData data = new UserData();
+
+            return data.GetUserById(userId);
+        }
+    }
+}
